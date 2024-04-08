a69ddf321274094af58108402cdf247cd095d122
jorsanchez76 <160294355+jorsanchez76@users.noreply.github.com>
2024-04-08 13:18:15 -0600

    Inicial

---
+++ config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  //port
+  PORT: process.env.PORT || 5000,
+
+  //secret key for API
+  SECRET_KEY: "BRwIpnT3R'{G8):",
+
+  //gmail credentials for send email
+  EMAIL: "jorge.sanchez.r76@gmail.com",
+  PASSWORD: "jhmc unay raqk odqn",
+
+  //secret key for jwt
+  JWT_SECRET: "PdvkTLyxc7",
+
+  //baseUrl
+  baseURL: "http://localhost/",
+
+  //firebase server key for notification
+  SERVER_KEY: "AAAABadCSHk:APA91bF8E_NeFW_yiHDgz8zlUtPqEkcHsaljobWsod6c7Pj7T_E8W-urhLrIO3VIWGtSvYXFnvavVkq_5Likv87d4lX6StAUBbdH8LqslXyQXyo_MPZDg4zZz0gQJvNCitmwSVwz_NXa",
+};
